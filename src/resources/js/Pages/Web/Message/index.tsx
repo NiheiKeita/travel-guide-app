70e8a13592f7cd0f522eab85a33baf486bdbfe44
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-02-24 00:07:23 +0900

    message画面作成

---
+++ src/resources/js/Pages/Web/Message/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import WebLayout from '@/Layouts/WebLayout'
+
+export const Message = React.memo(function Message() {
+
+    return (
+        <>
+            Message
+        </>
+    )
+})
+
+export default Message

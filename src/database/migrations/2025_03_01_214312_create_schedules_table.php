f9d1b89cdbd44462728e8142a144d8d3f6cd83e9
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-03-01 22:00:56 +0900

    マイグレーション作成

---
+++ src/database/migrations/2025_03_01_214312_create_schedules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger("travel_id");
+            $table->unsignedBigInteger("modal_id")->nullable();
+            $table->string('title')->nullable();
+            $table->timestamp('first_at')->nullable();
+            $table->timestamp('last_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};

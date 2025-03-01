c652767567b2987f827ca221302afc56de92885d
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-03-01 22:29:05 +0900

    Admin/TravelController作成

---
+++ src/app/Http/Controllers/Admin/TravelController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Travel;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class TravelController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('Admin/TravelListView', [
+            'travels' => Travel::get(),
+        ]);
+    }
+
+    public function show(Request $request): Response
+    {
+        return Inertia::render('Admin/TravelShowView', [
+            'travel' => Travel::where("id", $request->travel_id)->first(),
+        ]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('Admin/TravelCreateView');
+    }
+
+    public function save(): RedirectResponse
+    {
+        // $travel = new Travel();
+        // $travel->title = $request->title;
+        // $travel->description = $request->description;
+        // $travel->price = $request->price;
+        // $travel->image_id = $image->id;
+        // $travel->save();
+
+        return redirect(route("admin.travel.index"))->with('message', '登録が完了しました');
+    }
+
+    public function edit(Request $request): Response
+    {
+        return Inertia::render('Admin/TravelEditView', [
+            'travel' => Travel::where("id", $request->travel_id)->first(),
+        ]);
+    }
+
+    public function update(): RedirectResponse
+    {
+        // $travel = new Travel();
+        // $travel->title = $request->title;
+        // $travel->description = $request->description;
+        // $travel->price = $request->price;
+        // $travel->image_id = $image->id;
+        // $travel->save();
+
+        return redirect(route("admin.travel.index"))->with('message', '更新が完了しました');
+    }
+
+    public function delete(): RedirectResponse
+    {
+        // $travel = new Travel();
+        // $travel->title = $request->title;
+        // $travel->description = $request->description;
+        // $travel->price = $request->price;
+        // $travel->image_id = $image->id;
+        // $travel->save();
+
+        return redirect(route("admin.travel.index"))->with('message', '削除が完了しました');
+    }
+}
